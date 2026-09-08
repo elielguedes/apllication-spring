c9b941a792a473b5f2e9553de9bb7178fbc29792
Eliel Guedes <elielguedes817@gmail.com>
2026-09-08 19:21:34 -0300

    Servce layer

---
+++ src/main/java/com/projeto/project_spring/services/UseService.java
@@ -0,0 +1,25 @@
+package com.projeto.project_spring.services;
+
+import com.projeto.project_spring.entities.User;
+import com.projeto.project_spring.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UseService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> finAll(){
+        return repository.findAll();
+    }
+
+    public User findById(Long id){
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
