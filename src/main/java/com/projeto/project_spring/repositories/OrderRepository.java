217600cbd6162b1d97847f961d19979152054276
Eliel Guedes <elielguedes817@gmail.com>
2026-09-08 20:15:12 -0300

    Implements

--- src/main/java/com/projeto/project_spring/repositories/OrderRepository.java
+++ src/main/java/com/projeto/project_spring/repositories/OrderRepository.java
@@ -1,4 +1,8 @@
 package com.projeto.project_spring.repositories;
 
-public class OrderRepository {
+import com.projeto.project_spring.entities.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
 }
--- src/main/java/com/projeto/project_spring/services/OrderService.java
+++ src/main/java/com/projeto/project_spring/services/OrderService.java
@@ -1,4 +1,25 @@
 package com.projeto.project_spring.services;
 
+import com.projeto.project_spring.entities.Order;
+import com.projeto.project_spring.repositories.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class OrderService {
+
+    @Autowired
+    private OrderRepository repository;
+
+    public List<Order> finAll(){
+        return repository.findAll();
+    }
+
+    public Order findById(Long id){
+        Optional<Order> obj = repository.findById(id);
+        return obj.get();
+    }
 }
