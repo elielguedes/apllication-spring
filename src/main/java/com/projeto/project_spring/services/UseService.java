ae7cfb00b4294912a650b0ba515bbfa922c4dafc
Eliel Guedes <elielguedes817@gmail.com>
2026-09-10 15:22:17 -0300

    exce´tion handli update

--- src/main/java/com/projeto/project_spring/services/UseService.java
+++ src/main/java/com/projeto/project_spring/services/UseService.java
@@ -4,9 +4,9 @@ import com.projeto.project_spring.entities.User;
 import com.projeto.project_spring.repositories.UserRepository;
 import com.projeto.project_spring.services.exception.DataBaseException;
 import com.projeto.project_spring.services.exception.ResourceNotFoudException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -43,9 +43,14 @@ public class UseService {
     }
 
     public User update(Long id, User obj){
-        User entity = repository.getReferenceById(id); // pegar um objeto do banco de dados
-        updateData(entity, obj);
-        return repository.save(entity);
+        try{
+
+            User entity = repository.getReferenceById(id); // pegar um objeto do banco de dados
+            updateData(entity, obj);
+            return repository.save(entity);
+        }catch(EntityNotFoundException e){
+            throw new ResourceNotFoudException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
